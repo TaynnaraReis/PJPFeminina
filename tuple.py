d77f58f09531020bb2d6fee483b0af4f81451559
Taynnara Sousa <taynnara19187456@alunos.sc.senac.br>
2026-03-16 21:25:09 -0300

    primeira tuple

---
+++ tuple.py
@@ -0,0 +1,10 @@
+coordenadas = (10,20)
+dias = ('segunda', 'terça', 'quarta', 'quinta', 'sexta')
+
+print('x:', coordenadas[0])
+print('y:', coordenadas[1])
+print("Primeiros 3 dias:", dias[:3])
+print("Tamanho da tupla 'dias':", len(dias))
+print("Tem 'segunda'?", "segunda" in dias)
+print("Contagem de 'Terça':", dias.count("terça"))
+print("Índice de 'quarta':", dias.index("quarta"))
