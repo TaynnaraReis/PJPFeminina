72c6e8633de62d801ba19fd0fd74cdaf608eeeb2
Taynnara Sousa <taynnara19187456@alunos.sc.senac.br>
2026-03-30 20:51:14 -0300

    if elif else

---
+++ estrutura-condicional.py
@@ -0,0 +1,10 @@
+#entrada de dados
+nota = 5
+#classificação
+if nota >= 0 and nota < 3:
+    print('reprovado')
+elif nota >= 3 and nota < 7:
+    print('recuperação')
+else:
+    nota > 10
+    print('aprovado')
