61744d47fcf212a2e7ebed00f208243bda2b5e9a
Taynnara Sousa <taynnara19187456@alunos.sc.senac.br>
2026-03-30 21:18:37 -0300

    ativi2 estrutura

---
+++ atividade2-estrutura.py
@@ -0,0 +1,11 @@
+nota = float(input('Digite sua nota:'))
+
+if nota >= 9:
+    print('excelente')
+elif nota >= 7:
+    print('bom')
+elif nota >= 5:
+    print('regular')
+else:
+    nota < 5
+    print('reprovado')
